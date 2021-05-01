05b4d0cc61d75f65a68db2ab7006f791ed732c50
cloverfeng <clover.feng@tpisoftware.com>
2021-05-01 11:50:44 +0800

    建立棋子基底類別

---
+++ Gomoku.Client/Gomoku.Client/Abstract/PieceBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using Gomoku.Client.Enum;
+
+namespace Gomoku.Client.Abstract
+{
+    /// <summary>
+    /// 棋子抽象類別
+    /// </summary>
+    public abstract class PieceBase : PictureBox
+    {
+        /// <summary>
+        /// 基本棋子大小
+        /// </summary>
+        private const int baseSize = 50;
+
+        /// <summary>
+        /// 建構子
+        /// </summary>
+        /// <param name="x">棋子 x 座標</param>
+        /// <param name="y">棋子 y 座標</param>
+        public PieceBase(int x,int y)
+        {
+            this.BackColor = Color.Transparent;
+            this.Size = new Size(baseSize, baseSize);
+            this.Location = new Point(x - baseSize / 2, y - baseSize / 2);
+        }
+
+        /// <summary>
+        /// 取得棋子類型
+        /// </summary>
+        public abstract PieceType GetPieceType();
+    }
+}
---
+++ Gomoku.Client/Gomoku.Client/Enum/PieceType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gomoku.Client.Enum
+{
+    /// <summary>
+    /// 棋子類型列舉
+    /// </summary>
+    public enum PieceType
+    {
+        /// <summary>
+        /// 黑棋
+        /// </summary>
+        Black,
+        /// <summary>
+        /// 白棋
+        /// </summary>
+        White,
+        /// <summary>
+        /// 其他
+        /// </summary>
+        Other
+    }
+}
